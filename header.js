c66ed42c1b6217d8033df0dbffcb0c9a1ed345da
livemer <savosteevmark@gmail.com>
2025-06-04 17:30:09 +0300

    Мобилка

---
+++ header.js
@@ -0,0 +1,4 @@
+document.querySelector(".header-bottom__burger").addEventListener("click", function(){
+  document.querySelector(".header-bottom__burger").classList.toggle("burger-active")
+  document.querySelector(".header__nav").classList.toggle("header__nav-active")
+})
